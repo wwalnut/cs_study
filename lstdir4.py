1fec9ab47297895221daeb869aeceb163b429b89
tangramor <wangjh@a-li.com.cn>
2018-07-12 20:41:07 +0800

    fix code which lost return

--- lstdir4.py
+++ lstdir4.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-for num in range(1,101):
+# -*- coding: utf-8 -*-
+for num in range(1,101):
     for i in range(2,num):
         if (num%i==0):
             break
