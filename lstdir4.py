cfac9a09dc778011b1da791a09d1a8fbce2bc1e4
王子侨 <wangziqiao@Jack-Wangs-MacBook.local>
2018-07-19 19:51:35 +0800

    improve and readjust

--- lstdir4.py
+++ lstdir4.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-for num in range(1,101):
     for i in range(2,100):
         if (num%i2==0):
-        	print(num)
             break
         else:
+            print(num)
             break
