142044c74831aab93121235170be71696840cb3d
王子侨 <wangziqiao@Jack-Wangs-MacBook.local>
2018-08-04 19:49:52 +0800

    Changes

--- lstdir4.py
+++ lstdir4.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import time
+import math
 
 st = time.time()	# 起始时间
 
-last = 1000 # 设定要查找的范围：last
+last = 100 # 设定要查找的范围：last
 nums = list(True for _ in range(last + 1))	# 构造数量为 last + 1 个(因为第0个不记入)，元素值为 True 的数组
 
 # 根据step步长走过整个序列
@@ -17,7 +18,7 @@ def my_range(start, end, step):
 nums[0] = False # 0
 nums[1] = False # 1
 
-for i in range(2, last + 1):
+for i in range(2, 	int(math.sqrt(last + 1))):
 	for j in my_range(i, last + 1, i):
 		#print 'i', i, 'j', j # DEBUG: 查看i的倍数
 		if j > i and j <= last and nums[j]:
